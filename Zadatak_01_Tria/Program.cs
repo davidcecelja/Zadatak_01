68d88c6dcd3026e63f8a749e176708dc0e3cd54b
davidcecelja <46240796+davidcecelja@users.noreply.github.com>
2022-03-07 01:19:01 +0100

    Add project files.

---
+++ Zadatak_01_Tria/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace Zadatak_Tria
+{
+    internal class Program
+    {
+        private const string Pattern = @"(?:^|(?<![-0-9]))([0-9]+)";
+        static void Main()
+        {
+            int rezultat = 0;
+            string[] niz = new string[5];
+            niz = new string[5] { "asd5k55", "", "h-4jh5", "", "sde4h-6h7" };
+            foreach (string s in niz)
+            {
+                string[] input = Regex.Split(s, Pattern);
+                foreach (string _s in input)
+                {
+                    int broj;
+                    if (int.TryParse(_s, out broj))
+                    {
+                        rezultat += broj;
+                    }
+                }
+            }
+            Console.WriteLine("Zbroj je: {0}", rezultat);
+        }
+    }
+}
